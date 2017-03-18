fde7ad0ac6f640d5fab30611cc7e2b962699068f
Rodrigo Lpz <higuin_matador@hotmail.com>
2017-03-18 16:31:16 -0600

    Proyecto10

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    //recomendacion, poner comentarios a los codigos
+    //que se hagan
+    #comentario*/
+	//Rodrigo Lopez
+    /*comentario*/
+    echo "Hola mundo php";
+?>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <h1><p>Hola mundo</p></h1>
+    </body>
+</html>
